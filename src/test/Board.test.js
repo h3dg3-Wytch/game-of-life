d39c76e055d27a55718cab00a56427abcf73054b
Rex Petersen <rex_p_petersen@homedepot.com>
2020-05-01 17:04:47 -0500

    update board test

--- src/test/Board.test.js
+++ src/test/Board.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Board, {matrixToComponent, componentsToRows} from '../componets/Board';
+import Row from '../componets/Row';
 import Cell from '../componets/Cell';
 import sinon from 'sinon';
 
@@ -18,12 +19,15 @@ describe('Board', () => {
   const generateCellList = (n, m, cells) => {
     const result = [];
     for(let i = 0; i < n; i++) {
+      let row = [];
       for(let j = 0; j < m; j++) {
-        let cell = <Cell n={i} m={j} onClick={noop} active={cells[i][j]}/>
-        result.push(cell);
+        let cell = <Cell n={i} m={j} key={`${i},${j}`} handleClick={noop} active={cells[i][j]}/>
+        row.push(cell);
       }
+      result.push(row);
     }
-    return result;
+
+    return result.map(cells => <Row cells={cells}/>);
   }
 
   it('renders without crashing', () => {
@@ -32,34 +36,8 @@ describe('Board', () => {
 
   it('can render a matrix of cell components', () => {
     const result = matrixToComponent(sampleGrid, noop);
-    expect(result).toMatchObject(generateCellList(4, 3, sampleGrid));
-  });
-
-  it('can take those components and turn them into rows', () => {
-    const result = componentsToRows();
-    expect(result).toEqual(  
-    <div className="row">
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-      <div className="Cell"></div>
-    </div>);
+    const expected = generateCellList(4, 3, sampleGrid);
+    expect(result).toMatchObject(expected);
   });
 
 });
